58714676070ac8e8efe37439ca7ce82e0b71106f
charlottegracia <1086120@ucn.dk>
2021-02-16 15:19:57 +0100

    landingpage + fixed bottom nav

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+function showCykeltur() {
+    document.getElementById('vandretur').style.display = 'none';
+    document.getElementById('cykeltur').style.display = 'block';
+}
+
+function showVandretur() {
+    document.getElementById('vandretur').style.display = 'block';
+    document.getElementById('cykeltur').style.display = 'none';
+}
