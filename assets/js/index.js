64ead6a93e4e4f2f954743b079ff19bb930f611f
charlottegracia <1086120@ucn.dk>
2021-02-19 13:01:28 +0100

    før ikoner

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,3 +7,50 @@ function showVandretur() {
     document.getElementById('vandretur').style.display = 'block';
     document.getElementById('cykeltur').style.display = 'none';
 }
+
+function mulbjergInfo() {
+    document.getElementById('startside').style.display = 'none';
+    document.getElementById('mulbjerge').style.display = 'block';
+    document.getElementById('pil').style.display = 'block';
+}
+
+function poulstrupInfo() {
+    document.getElementById('startside').style.display = 'none';
+    document.getElementById('poulstrup').style.display = 'block';
+    document.getElementById('pil').style.display = 'block';
+}
+
+function egholmInfo() {
+    document.getElementById('startside').style.display = 'none';
+    document.getElementById('egholm').style.display = 'block';
+    document.getElementById('pil').style.display = 'block';
+}
+
+function oesteraadalenNordInfo() {
+    document.getElementById('startside').style.display = 'none';
+    document.getElementById('oesteraadalenNord').style.display = 'block';
+    document.getElementById('pil').style.display = 'block';
+}
+
+function oesteraadalenSydInfo() {
+    document.getElementById('startside').style.display = 'none';
+    document.getElementById('oesteraadalenSyd').style.display = 'block';
+    document.getElementById('pil').style.display = 'block';
+}
+
+function bjergbanestienInfo() {
+    document.getElementById('startside').style.display = 'none';
+    document.getElementById('bjergbanestien').style.display = 'block';
+    document.getElementById('pil').style.display = 'block';
+}
+
+function tilbageTilStartside() {
+    document.getElementById('pil').style.display = 'none';
+    document.getElementById('mulbjerge').style.display = 'none';
+    document.getElementById('poulstrup').style.display = 'none';
+    document.getElementById('egholm').style.display = 'none';
+    document.getElementById('oesteraadalenNord').style.display = 'none';
+    document.getElementById('oesteraadalenSyd').style.display = 'none';
+    document.getElementById('bjergbanestien').style.display = 'none';
+    document.getElementById('startside').style.display = 'block';
+}
